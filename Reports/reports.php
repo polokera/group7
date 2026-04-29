5a39ecb7395fc683d270c9ac389b45048f6b5acc
unknown <Stephenchiundira@gmail.com>
2026-04-29 18:59:29 +0200

    UI for the reports dashboard

---
+++ Reports/reports.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="../css/Reports.css">
+        <title>Reports</title>  
+    </head>
+    <body>
+        <div class="ReportBoxes">
+        <div class="UsageBox">
+           Member usage report 
+           <table>
+                <thead>
+                    <tr>
+                        <th>Request Date</th>
+                        <th>Destination</th>
+                        <th>Vehicle Type</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>31/03/2026</td>
+                        <td>Blantyre</td>
+                        <td>Saloon</td>
+                        <td><span class="approved-status">Approved by Registrar</span></td>
+                        <td><button class="pdf-download">Download PDF Report</button></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class= "OverviewBox">
+            Overview
+        </div>
+        </div>
+        
+    </body>
+</html>
