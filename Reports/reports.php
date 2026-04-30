795e688b7c3a3ebb6e9405c684d3c8f84c9495e3
unknown <Stephenchiundira@gmail.com>
2026-04-30 20:43:03 +0200

    Started the pdf generation in reports.php

--- Reports/reports.php
+++ Reports/reports.php
@@ -1,3 +1,21 @@
+<?php require_once '../vendor/autoload.php';
+
+            use Dompdf\Dompdf;
+
+            // instantiate and use the dompdf class
+            $dompdf = new Dompdf();
+            $dompdf->loadHtml('hello world');
+
+            // (Optional) Setup the paper size and orientation
+            $dompdf->setPaper('A4', 'landscape');
+
+            // Render the HTML as PDF
+            $dompdf->render();
+
+            // Output the generated PDF to Browser
+            $dompdf->stream();
+        ?>
+
 <!DOCTYPE html>
 <html>
     <head>
